1c9b418de23b8f3ef415f26daeccbc39dc338c60
gesinski <147282358+gesinski@users.noreply.github.com>
2024-10-28 16:05:27 +0100

    lab2

--- 2D Platformer 61/Assets/Scripts/PlayerController.cs
+++ 2D Platformer 61/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,36 +13,86 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidbody;
     public LayerMask groundLayer;
     const float rayLength = 1.5f;
+    private Animator animator;
+    private bool isWalking = false;
+    private bool isFacingRight = true;
+    private int score = 0;
+    private bool isLadder = false;
+    private bool isClimbing = false;
+    private float vertical;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+        isWalking = false;
+        vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            isWalking = true;
+            if (!isFacingRight)
+            {
+                Flip();
+            }
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             transform.Translate(moveSpeed * Time.deltaTime * -1.0f, 0.0f, 0.0f, Space.World);
+            isWalking = true;
+            if (isFacingRight)
+            {
+                Flip();
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
-        Debug.DrawRay(transform.position, rayLength*Vector3.down, Color.white);
+
+        if (isLadder && Math.Abs(vertical) > 0)
+        {
+            isClimbing = true;
+        }
+        //Debug.DrawRay(transform.position, rayLength*Vector3.down, Color.white);
+
+        animator.SetBool("isGrounded", IsGrounded());
+        animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isClimbing", isClimbing);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rigidbody.gravityScale = 0;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, vertical * moveSpeed);
+        } else
+        {
+            rigidbody.gravityScale = 1;
+        }
     }
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-    
+
     bool IsGrounded()
     {
         return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
@@ -61,6 +113,24 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Player fell into void");
         }
+        if (col.CompareTag("Bonus"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            col.gameObject.SetActive(false);
+        }
+        if (col.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
     }
 
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
 }
